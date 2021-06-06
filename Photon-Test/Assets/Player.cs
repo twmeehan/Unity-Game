cb083426351e54c84a0f3c26373a5a79674dcf31
Timothy <tmeehan26@gmail.com>
2021-06-06 18:12:52 -0400

    Added Movement
    - Character prefab created on joining a room

---
+++ Photon-Test/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 100;
+    PhotonView view;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        view = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (view.IsMine)
+        {
+
+        }
+        Vector3 input = new Vector3(Input.mousePosition.x - transform.position.x, Input.mousePosition.y - transform.position.y, 0);
+        transform.position += input * speed * Time.deltaTime;
+
+    }
+}
