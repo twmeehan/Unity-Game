33355d6f38ecbf13b712d479c79e0b5c520eb8a3
Timothy <tmeehan26@gmail.com>
2021-06-14 21:55:26 -0400

    Prep for multiplayer physics testing
    all Kyle's work merged with main branch

---
+++ Photon-Test/Assets/Scripts/Loading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("1");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("2");
+
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("3");
+
+    }
+}
---
+++ Photon-Test/Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    public float minX, maxX, minY, maxY;
+
+    public void Start()
+    {
+        Vector2 randomPos = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        PhotonNetwork.Instantiate(playerPrefab.name, randomPos, Quaternion.identity);
+    }
+}
