2024bf53beacd0a8bd48ef5fd2874e5c25e385e9
Timothy <tmeehan26@gmail.com>
2021-06-24 19:59:43 -0400

    Added Photon Stuff

--- Photon-Test/Assets/Scripts/playerScript.cs
+++ Photon-Test/Assets/Scripts/playerScript.cs
@@ -8,10 +8,19 @@ public class playerScript : MonoBehaviourPunCallbacks, IPunObservable
 
     public float speed;
     public float jumpForce;
+    public float grav = -3;
+    public static float dragConstant = 0.2f;
+    public Vector2 drag = new Vector2(0,0);
 
+    // For Jump
+    public Transform IsGroundedChecker;
+    public static float CheckGroundRadius = 0.18f;
+    public LayerMask GroundLayer;
+
+    [SerializeField] private Vector2 accel = new Vector2(0,0);
     [SerializeField] private Vector2 vel = new Vector2(0,0);
     [SerializeField] private Vector2 pos = new Vector2(0,0);
-
+    
     private Transform transform;
     private Rigidbody2D rb;
     private PhotonView view;
@@ -24,7 +33,7 @@ public class playerScript : MonoBehaviourPunCallbacks, IPunObservable
         rb = this.gameObject.GetComponent<Rigidbody2D>();
         view = this.gameObject.GetComponent<PhotonView>();
 
-        PhotonNetwork.SerializationRate = 5;
+        PhotonNetwork.SerializationRate = 13;
 
     }
 
@@ -33,17 +42,34 @@ public class playerScript : MonoBehaviourPunCallbacks, IPunObservable
     {
         if (view.IsMine)
         {
-            pos = transform.position;
-            vel = rb.velocity;
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0)
             {
-                rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
+                rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * speed, vel.y);
+                
             }
+            drag = dragConstant * vel * -1;
+            if (CheckIfGrounded())
+            {
+                accel = new Vector2(0, grav) * 0.5f + drag;
+            } else
+            {
+                accel = new Vector2(0, grav) + drag;
+                Debug.Log("Grav");
+            }
+            
+            rb.velocity += accel * Time.deltaTime;
+            vel = rb.velocity;
+            pos = transform.position;
+
         } else
         {
             transform.position = pos;
+            //vel += accel * Time.deltaTime;
             rb.velocity = vel;
+
         }
+        
+
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -52,11 +78,29 @@ public class playerScript : MonoBehaviourPunCallbacks, IPunObservable
         {
             stream.SendNext(vel);
             stream.SendNext(pos);
+            stream.SendNext(accel);
         } else
         {
             vel = (Vector2) stream.ReceiveNext();
             pos = (Vector2) stream.ReceiveNext();
+            accel = (Vector2) stream.ReceiveNext();
+            float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.timestamp));
+            pos += 0.5f * accel * lag * lag + vel * lag;
         }
+    }
+    bool CheckIfGrounded()
+    {
+
+        Collider2D collider = Physics2D.OverlapCircle(IsGroundedChecker.position, CheckGroundRadius, GroundLayer);
+        if (collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 
 }
