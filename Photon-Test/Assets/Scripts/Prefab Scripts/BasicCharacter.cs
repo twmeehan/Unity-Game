f436b192f0dfb0a6bbee737ed0885b057159a42c
Timothy <tmeehan26@gmail.com>
2021-11-18 20:21:57 -0500

    Added Beds

--- Photon-Test/Assets/Scripts/Prefab Scripts/BasicCharacter.cs
+++ Photon-Test/Assets/Scripts/Prefab Scripts/BasicCharacter.cs
@@ -5,19 +5,36 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BasicCharacter : MonoBehaviour
+public class BasicCharacter : MonoBehaviour, IPunObservable
 {
     Transform transform;
     Rigidbody2D rb;
     PhotonView view;
+
+    // variable constants
     public float speed;
     public float jump_strength;
+
+    // layers
     public LayerMask GroundLayer;
     public LayerMask RoomLayer;
+    public LayerMask BedLayer;
+
+    // random Objects
     public GameObject camera;
     public TextMeshProUGUI name;
+    public Button button;
 
-    float deltaTime;
+    // private variables
+    private bool infected = false;
+    private char room;
+    private bool sleeping = false;
+    private BedScript bed;
+    private int buttonType = 0;
+
+    private int DISABLED = 0;
+    private int GET_INTO_BED = 1;
+    private int LEAVE_BED = 2;
 
 
 
@@ -40,56 +57,155 @@ public class BasicCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (view.IsMine) {
+        RaycastHit2D currentRoom = Physics2D.Raycast(transform.position, Vector2.up, 0.1f, RoomLayer);
+        if (currentRoom.collider != null)
+        {
+            room = currentRoom.collider.gameObject.name[0];
+        }
+        if (view.IsMine && !sleeping) {
 
-            bool isGrounded = false;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, GroundLayer);
-            if (hit.collider != null)
+            RaycastHit2D currentBed = Physics2D.Raycast(transform.position, Vector2.up, 0.1f, BedLayer);
+            if (currentBed.collider != null)
             {
 
-                isGrounded = true;
+                button.interactable = true;
+                buttonType = GET_INTO_BED;
+                //button.image.sprite=...
 
-            }
-            RaycastHit2D room = Physics2D.Raycast(transform.position, Vector2.up, 0.1f, RoomLayer);
-            if (room.collider != null)
-            {
-                Debug.Log(room.collider.gameObject.name);
-            }
-
-            if (Input.GetKey(KeyCode.Space) && isGrounded)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jump_strength);
-            }
-            if (rb.velocity.y < 0)
-            {
-                rb.gravityScale = 6;
             }
             else
             {
-                rb.gravityScale = 2;
-            }
-            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
-            {
-                rb.velocity = new Vector2(0, rb.velocity.y);
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                rb.velocity = new Vector2(-1 * speed , rb.velocity.y);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                rb.velocity = new Vector2(speed, rb.velocity.y);
-            } else
-            {
-                rb.velocity = new Vector2(0, rb.velocity.y);
+
+                button.interactable = false;
+                buttonType = DISABLED;
+                //button.image.sprite=...
 
             }
 
-            deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-            float fps = 1.0f / deltaTime;
-            Debug.Log(Mathf.Ceil(fps).ToString());
+            move();
 
         }
         
+    }
+    // Called by BedScript when a player successfully gets into a bed
+    [PunRPC]
+    public void JoinBed(BedScript s, Transform t)
+    {
+
+        sleeping = true;
+        rb.velocity = new Vector2(0, 0);
+        rb.gravityScale = 0;
+
+        // Move player to the center of bed
+        this.transform.position = t.position;
+
+        //anim.SetBool("Sleeping");
+
+        bed = s;
+
+        button.enabled = true;
+        //button.image.sprite=...
+        buttonType = LEAVE_BED;
+    }
+
+    [PunRPC]
+    // Called by BedScript whenever this player leaves a bed
+    public void KickFromBed()
+    {
+
+        // TODO: Play animation or add particles or smth
+        sleeping = false;
+        bed = null;
+
+    }
+
+    // Called when the player presses the button to leave bed
+    public void WakeUp()
+    {
+        bed.LeaveBed();
+    }
+
+    // Called when the player presses the button to enter bed
+    public void Sleep()
+    {
+
+        RaycastHit2D currentBed = Physics2D.Raycast(transform.position, Vector2.up, 0.1f, BedLayer);
+        currentBed.collider.gameObject.GetComponent<BedScript>().EnterBed(this.gameObject);
+
+    }
+    public void ButtonPressed()
+    {
+        switch (buttonType)
+        {
+            case 0:
+                Debug.Log("Error");
+                break;
+            case 1:
+                Sleep();
+                break;
+            case 2:
+                WakeUp();
+                break;
+        }
+    }
+    void move()
+    {
+        bool isGrounded = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, GroundLayer);
+        if (hit.collider != null)
+        {
+
+            isGrounded = true;
+
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_strength);
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = 6;
+        }
+        else
+        {
+            rb.gravityScale = 2;
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1 * speed, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
+        if (stream.IsWriting)
+        {
+            stream.SendNext(sleeping);
+            stream.SendNext(infected);
+            stream.SendNext(bed);
+            stream.SendNext(room);
+        }
+        else
+        {
+            sleeping = (bool) stream.ReceiveNext();
+            infected = (bool) stream.ReceiveNext();
+            bed = (BedScript) stream.ReceiveNext();
+            room = (char) stream.ReceiveNext();
+        }
     }
 }
---
+++ Photon-Test/Assets/Scripts/Prefab Scripts/BedScript.cs
@@ -0,0 +1,73 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedScript : MonoBehaviour, IPunObservable
+{
+
+    public GameObject covers;
+    public Transform transform;
+
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // The covers are disabled so there is no breathing animation playing when the bed is created
+        covers.GetComponent<SpriteRenderer>().enabled = false;
+
+    }
+
+    // Called by the player when they want to join this bed
+    public void  EnterBed(GameObject p)
+    {
+
+        // If there is a player in this bed, yeet them
+        if (this.player != null)
+            player.GetComponent<BasicCharacter>().KickFromBed();
+
+        // The new player joins the bed and BasicCharacter.JoinBed() tells the player to start sleeping animations
+        player = p;
+        p.GetComponent<BasicCharacter>().JoinBed(this, transform);
+
+        // The covers are enabled and begin playing a breathing animation
+        covers.GetComponent<SpriteRenderer>().enabled = true;
+
+    }
+    // Called by the player when they want to leave their bed
+    public void LeaveBed()
+    {
+
+        // If there is a player in this bed, yeet them
+        if (this.player != null)
+            player.GetComponent<BasicCharacter>().KickFromBed();
+        player = null;
+
+        // The covers are disabled so there is no breathing animation playing
+        covers.GetComponent<SpriteRenderer>().enabled = false;
+
+    }
+
+    // Used to get the object that contains the BasicCharacter script for the player that is sleeping
+    public GameObject getPlayer()
+    {
+        return player;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
+        if (stream.IsWriting)
+        {
+            stream.SendNext(player);
+            stream.SendNext(covers);
+        }
+        else
+        {
+            player = (GameObject) stream.ReceiveNext();
+            covers = (GameObject)stream.ReceiveNext();
+        }
+    }
+}
